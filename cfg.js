0a3215d6bc579ff74e6bdc8203a84d6edaf12d8c
zhufengning <zhufengning18@gmail.com>
2017-09-29 12:22:47 +0800

    Add files via upload

---
+++ cfg.js
@@ -0,0 +1,5 @@
+var set={
+'timeout':5000,
+'background':'bg.jpg',
+'apis':['https://hitoapi.cc/sp/','https://api.i-meto.com/hitokoto?encode=json','https://api.lwl12.com/hitokoto/main/get?encode=json']
+}
